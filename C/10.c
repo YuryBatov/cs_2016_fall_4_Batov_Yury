6565487fe039047563336ea7f2e4841a34f26738
YuryBatov <batov1998@list.ru>
2017-04-14 19:08:47 +0300

    Update 10.c

--- C/10.c
+++ C/10.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
-#include <string.h>
 #include <locale.h>
+
+int pow(int a, int step)
+{
+    int i;
+    int s=a;
+	if (step == 0)
+		return 1;
+	for ( i=1; i<step; i++)
+    {
+        s*=a;
+    }
+    return s;
+}
+
 int rightrot(int x,int n)
 {
-return (x>>n);
+    int c;
+    c=n%32;
+    n=32-c;
+    return x=x*pow(2,n);
 }
+
 int main()
 {
     setlocale(LC_ALL,"Rus");
@@ -13,7 +30,6 @@ int main()
     scanf("%d",&b);
     printf("Введите разряд\n");
     scanf("%d",&h);
-    f=rightrot(b,h);
-    printf("%i",f);
+    printf("Результат %u\n",rightrot(b,h));
     return 0;
 }
